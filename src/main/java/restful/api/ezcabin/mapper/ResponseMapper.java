2c4da3a691a5333ae110433fe1557f4c13d3bb14
bintangginanjar <bintang.ginanjar@gmail.com>
2025-02-16 19:03:21 +0700

    Add jwt configuration

---
+++ src/main/java/restful/api/ezcabin/mapper/ResponseMapper.java
@@ -0,0 +1,5 @@
+package restful.api.ezcabin.mapper;
+
+public class ResponseMapper {
+
+}
--- src/main/java/restful/api/ezcabin/repository/UserRepository.java
+++ src/main/java/restful/api/ezcabin/repository/UserRepository.java
@@ -1,9 +1,15 @@
 package restful.api.ezcabin.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import restful.api.ezcabin.entity.UserEntity;
 
 public interface UserRepository extends JpaRepository<UserEntity, Long>{
     
+    Optional<UserEntity> findByUsername(String username);
+
+    Optional<UserEntity> findFirstByToken(String token);
+
 }
